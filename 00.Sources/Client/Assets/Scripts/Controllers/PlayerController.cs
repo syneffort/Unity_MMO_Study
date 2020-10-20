93fe89589a139bff9c849e6ef97f1d2902d8a130
Pyunghwa Kim <syneffort@gmail.com>
2020-10-21 07:27:34 +0900

    Player Move

---
+++ 00.Sources/Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class PlayerController : MonoBehaviour
+{
+    public Grid _grid;
+    public float _speed = 5.0f;
+
+    Vector3Int _cellPos = Vector3Int.zero;
+    MoveDir _dir = MoveDir.None;
+    bool _isMoving = false;
+
+    void Start()
+    {
+        Vector3 pos = _grid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f);
+        transform.position = pos;
+    }
+
+    void Update()
+    {
+        GetDirInput();
+        UpdatePosition();
+        UpdateIsMoving();
+    }
+
+    void GetDirInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+            _dir = MoveDir.Up;
+        else if (Input.GetKey(KeyCode.S))
+            _dir = MoveDir.Down;
+        else if (Input.GetKey(KeyCode.A))
+            _dir = MoveDir.Left;
+        else if (Input.GetKey(KeyCode.D))
+            _dir = MoveDir.Right;
+        else
+            _dir = MoveDir.None;
+    }
+
+    void UpdatePosition()
+    {
+        if (!_isMoving)
+            return;
+
+        Vector3 destPos = _grid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f);
+        Vector3 moveDir = destPos - transform.position;
+
+        float dist = moveDir.magnitude;
+        if (dist < _speed * Time.deltaTime)
+        {
+            transform.position = destPos;
+            _isMoving = false;
+        }
+        else
+        {
+            transform.position += moveDir.normalized * _speed * Time.deltaTime;
+            _isMoving = true;
+        }
+        
+    }
+
+    void UpdateIsMoving()
+    {
+        if (!_isMoving)
+        {
+            switch (_dir)
+            {
+                case MoveDir.Up:
+                    _cellPos += Vector3Int.up;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Down:
+                    _cellPos += Vector3Int.down;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Left:
+                    _cellPos += Vector3Int.left;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Right:
+                    _cellPos += Vector3Int.right;
+                    _isMoving = true;
+                    break;
+            }
+        }
+    }
+}
