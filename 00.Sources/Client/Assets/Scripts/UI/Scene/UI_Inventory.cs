0331d0b8b9144aacd6020aeabbd90295a08b939e
Pyunghwa Kim <syneffort@gmail.com>
2021-01-26 10:22:49 +0900

    Inventory #1

---
+++ 00.Sources/Client/Assets/Scripts/UI/Scene/UI_Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Inventory : UI_Base
+{
+    public List<UI_Inventory_Item> Items { get; } = new List<UI_Inventory_Item>();
+
+    public override void Init()
+    {
+        Items.Clear();
+
+        GameObject grid = transform.Find("ItemGrid").gameObject;
+        foreach (Transform child in grid.transform)
+            Destroy(child.gameObject);
+
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject go = Managers.Resource.Instantiate("UI/Scene/UI_Inventory_Item", grid.transform);
+            UI_Inventory_Item item = go.GetOrAddComponent<UI_Inventory_Item>();
+            Items.Add(item);
+        }
+    }
+
+    public void RefreshUI()
+    {
+
+    }
+}
